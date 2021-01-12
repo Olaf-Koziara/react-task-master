d14242acdd54cfd60b2d71ca13a362a526f1a9b8
olafk <olaf.koziara@gmail.com>
2021-01-12 23:36:39 +0100

    add cart

---
+++ front/src/views/Cart/Cart.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import CartListItem from "../../components/CartListItem/CartListItem";
+import {
+  StyledCartList,
+  StyledButton,
+  StyledCartWrapper,
+} from "../../styled_components";
+
+const Cart = ({ cartCounter, cartItems }) => {
+  return (
+    <StyledCartWrapper>
+      <StyledCartList>
+        {cartItems.length > 0 ? (
+          <>
+            {" "}
+            {cartItems.map((item) => (
+              <CartListItem cartItem={item} />
+            ))}
+          </>
+        ) : (
+          <div>Brak przedmiotów w koszyku</div>
+        )}
+      </StyledCartList>
+      {cartItems.length > 0 ? (
+        <Link to="/order">
+          <StyledButton>Przejdz dalej</StyledButton>
+        </Link>
+      ) : null}
+    </StyledCartWrapper>
+  );
+};
+const mapStateToProps = (state) => ({
+  cartCounter: state.cartCounter,
+  cartItems: state.cartItems,
+});
+const mapDispatchToProps = (dispatch) => ({});
+export default connect(mapStateToProps, mapDispatchToProps)(Cart);
