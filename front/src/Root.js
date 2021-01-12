9dde8c884dcba8b45b276977a6d4c6de880ea3be
olafk <olaf.koziara@gmail.com>
2021-01-12 20:13:53 +0100

    redux connect Root

--- front/src/Root.js
+++ front/src/Root.js
@@ -3,16 +3,26 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Provider, connect } from "react-redux";
 import { store } from "./store";
 import axios from "axios";
-const Root = () => {
+import { setBooksAction } from "./actions";
+const Root = ({ books, setBooks }) => {
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/api/book")
+      .then((response) => setBooks(response.data));
+  }, []);
+
   return (
-    <Provider store={store}>
-      <Router>
-        <Switch>
-          <Route path="/" exact />
-        </Switch>
-      </Router>
-    </Provider>
+    <Router>
+      <Switch>
+        <Route path="/" exact />
+      </Switch>
+    </Router>
   );
 };
-
-export default Root;
+const mapStateToProps = (state) => ({
+  books: state.booksArray,
+});
+const mapDispatchToProps = (dispatch) => ({
+  setBooks: (books) => dispatch(setBooksAction(books)),
+});
+export default connect(mapStateToProps, mapDispatchToProps)(Root);
