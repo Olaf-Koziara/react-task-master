80024713d453e777977ad3f9ea4f934473873b01
olafk <olaf.koziara@gmail.com>
2021-01-12 19:52:43 +0100

    add set book action

---
+++ front/src/actions/actionTypes.js
@@ -0,0 +1,3 @@
+export const actionTypes = {
+  setBooks: "SET_BOOKS",
+};
--- front/src/reducers/index.js
+++ front/src/reducers/index.js
@@ -1,7 +1,15 @@
-const initialState = {};
+import { actionTypes } from "../actions/actionTypes";
+
+const initialState = { booksArray: [] };
 export const reactTaskReducer = (state = initialState, action) => {
   const { type, payload } = action;
   switch (type) {
+    case actionTypes.setBooks: {
+      return {
+        ...state,
+        booksArray: payload,
+      };
+    }
     default:
       return state;
   }
