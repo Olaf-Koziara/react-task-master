205bcd56ed3d202b98f884f333d31d2edc895c0e
olafk <olaf.koziara@gmail.com>
2021-01-12 18:43:56 +0100

    add routing

--- front/src/Root.js
+++ front/src/Root.js
@@ -1,7 +1,13 @@
 import React from "react";
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 const Root = () => {
-  return <div></div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact />
+      </Switch>
+    </Router>
+  );
 };
 
 export default Root;
