e4b304f1eb4edd38af9e269c1cdeb365fcac6bdd
olafk <olaf.koziara@gmail.com>
2021-01-13 01:14:53 +0100

    add local storage functionality

--- front/src/Root.js
+++ front/src/Root.js
@@ -1,31 +1,59 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Provider, connect } from "react-redux";
 import { store } from "./store";
 import axios from "axios";
-import { setBooksAction } from "./actions";
-import MainTemplate from "../templates/MainTemplate/MainTemplate";
-const Root = ({ books, setBooks }) => {
+import { setBooksAction, setCartItemsAction } from "./actions";
+import MainTemplate from "./templates/MainTemplate/MainTemplate";
+import Home from "./views/Home/Home";
+import Cart from "./views/Cart/Cart";
+import Order from "./views/Order/Order";
+const Root = ({
+  booksData,
+  setBooks,
+  selectedPage,
+  cartItems,
+  cartCounter,
+  setCartItems,
+}) => {
   useEffect(() => {
-    axios
-      .get("http://localhost:3001/api/book")
-      .then((response) => setBooks(response.data));
+    const cartItems = localStorage.getItem("cartItems");
+
+    if (cartItems) {
+      setCartItems(JSON.parse(cartItems));
+    }
   }, []);
+  useEffect(() => {
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+  }, [cartItems]);
+  useEffect(() => {
+    if (selectedPage) {
+      axios
+        .get(`http://localhost:3001/api/book/?page${selectedPage}`)
+        .then((response) => setBooks(response.data));
+    }
+  }, [selectedPage]);
 
   return (
     <Router>
       <MainTemplate>
         <Switch>
-          <Route path="/" exact />
+          <Route path="/" exact component={Home} />
+          <Route path="/cart" component={Cart} />
+          <Route path="/order" component={Order} />
         </Switch>
       </MainTemplate>
     </Router>
   );
 };
 const mapStateToProps = (state) => ({
-  books: state.booksArray,
+  booksData: state.booksArray,
+  selectedPage: state.selectedPage,
+  cartItems: state.cartItems,
+  cartCounter: state.cartCounter,
 });
 const mapDispatchToProps = (dispatch) => ({
   setBooks: (books) => dispatch(setBooksAction(books)),
+  setCartItems: (items) => dispatch(setCartItemsAction(items)),
 });
 export default connect(mapStateToProps, mapDispatchToProps)(Root);
