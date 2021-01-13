bd7849a6a306d2f573512c8c3fac4b371e2ea824
olafk <olaf.koziara@gmail.com>
2021-01-13 01:14:26 +0100

    add order view

---
+++ front/src/views/Order/Order.js
@@ -0,0 +1,12 @@
+import React from "react";
+import OrderForm from "../../components/OrderForm/OrderForm";
+
+const Order = () => {
+  return (
+    <div className="text-center">
+      <OrderForm />
+    </div>
+  );
+};
+
+export default Order;
