d6160a8bc2ef0c2142c0171d47ee02762ac3ecda
olafk <olaf.koziara@gmail.com>
2021-01-13 14:00:35 +0100

    add no data warning

--- front/src/views/Home/Home.js
+++ front/src/views/Home/Home.js
@@ -5,19 +5,24 @@ import { StyledBookList } from "../../styled_components";
 
 const Home = ({ books }) => {
   return (
-    <div>
-      {books ? (
-        <StyledBookList>
-          {books.map((book) => (
-            <BookListItem key={book.id} bookData={book} />
-          ))}
-        </StyledBookList>
-      ) : null}
-    </div>
+    <>
+      <div>
+        {books ? (
+          <StyledBookList>
+            {books.map((book) => (
+              <BookListItem key={book.id} bookData={book} />
+            ))}
+          </StyledBookList>
+        ) : (
+          <div className="text-center">Brak danych</div>
+        )}
+      </div>
+    </>
   );
 };
 const mapStateToProps = (state) => ({
   books: state.booksArray.data,
+  apiData: state.booksArray.metadata,
 });
 const mapDispatchToProps = (dispatch) => ({});
 export default connect(mapStateToProps, mapDispatchToProps)(Home);
